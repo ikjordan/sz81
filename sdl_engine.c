8325778b0ba066f01b785dc6d9dcaa395c1c35f9
ikjordan <iankjordan@gmail.com>
2024-01-27 12:50:18 +0000

    Risc OS build

--- sdl_engine.c
+++ sdl_engine.c
@@ -199,9 +199,8 @@ int sdl_init(void) {
 	 * setting a video mode as per SDL docs instructions */
 	#if defined(PLATFORM_GP2X)
 	#elif defined(PLATFORM_ZAURUS)
-	#elif defined(PLATFORM_RISCOS)
-		SDL_WM_SetCaption("sz81 2.3.12", "sz81");
 	#else
+    #ifndef PLATFORM_RISCOS
 		strcpy(filename, PACKAGE_DATA_DIR);
 		strcatdelimiter(filename);
 		strcat(filename, IMG_WM_ICON);
@@ -213,9 +212,9 @@ int sdl_init(void) {
 		} else {
 			SDL_WM_SetIcon (wm_icon, NULL);
 		}
-
+	#endif
 		/* Set display window title */
-		SDL_WM_SetCaption("sz81", "sz81");
+		SDL_WM_SetCaption("sz81 "VERSION, "sz81");
 	#endif
 
 	/* Set-up the local data directory */
